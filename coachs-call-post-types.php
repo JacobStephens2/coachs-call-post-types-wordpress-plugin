7fc2907f38b937ef8e80f96ed8d704a70edc99f7
Jacob Stephens <jacob@stewardgoods.com>
2021-11-23 17:24:01 -0500

    add plugin overview details

---
+++ coachs-call-post-types.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name:       Coach's Call Post Types and Taxonomies
+ * Plugin URI:        https://github.com/JacobStephens2/coachs-call-post-types
+ * Description:       A simple plugin for creating custom post types and taxonomies related to a Coach's Call.
+ * Version:           1.0.0
+ * Author:            Jacob Stephens
+ * Author URI:        https:/stewardgoods.com/
+ * License:           GPLv3
+ * License URI:       https://github.com/JacobStephens2/coachs-call-post-types/blob/main/LICENSE.txt
+ * Text Domain:       coachs-call-post-types
+ * Domain Path:       /languages
+ */
+
+
