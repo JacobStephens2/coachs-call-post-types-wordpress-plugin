7ee5c0bcd6ebc4569a5add8c950759998eff3c28
Jacob Stephens <jacob@stewardgoods.com>
2021-11-23 19:00:58 -0500

    show coach quotes cpt in graphql

--- post-types/register.php
+++ post-types/register.php
@@ -11,6 +11,9 @@ function cc_register_coach_quote_type() {
     'public'         => true,
     'show_in_rest'	 => true,
     'rest_base'	     => 'coach-quotes',
+    'show_in_graphql' => true,
+    'graphql_single_name' => 'coach-quote',  
+    'graphql_plural_name' => 'coach-quotes',
   );
 
   register_post_type( 'coach_quote', $args );
